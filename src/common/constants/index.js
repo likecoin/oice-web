e7e43edbad8fc6a900806b88f62b34f1511eccb5
David Ng <nwingt@gmail.com>
2017-10-30 15:28:44 +0800

    🔖 Update version to 4.3.0

--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -15,7 +15,7 @@ import {
     INTERCOM_APP_ID_TEST,
 } from './key';
 
-export const VERSION = '4.2.0';
+export const VERSION = '4.3.0';
 
 export const SRV_ENV = process.env.SRV_ENV;
 export const IS_CLIENT = process.env.IS_CLIENT;
