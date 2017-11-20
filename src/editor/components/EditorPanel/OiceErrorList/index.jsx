a1a7a3472b011f5c99e3fafc5f512d0e301625c3
Ng Wing Tat (David) <i@ngwingt.at>
2017-11-20 13:11:19 +0800

    💄👽 Better validation on building oice (#44)

--- src/editor/components/EditorPanel/OiceErrorList/index.jsx
+++ src/editor/components/EditorPanel/OiceErrorList/index.jsx
@@ -3,35 +3,73 @@ import PropTypes from 'prop-types';
 
 import './styles.scss';
 
+
+function ErrorAttributeList(props) {
+  const { blockId, blockIndex, macroName, errors, t } = props;
+  return (
+    <ul className="error-attribute-list">
+      {errors.map(({ attributeName, code, value }) => (
+        <li key={attributeName} className="error-attribute-item">
+          <table>
+            <tr>
+              <td><span>{t('oiceError.errorAttribute')}</span></td>
+              <td><span>{t(`macro:${macroName}.${attributeName}`)}</span></td>
+            </tr>
+            <tr>
+              <td><span>{t('oiceError.messageLabel')}</span></td>
+              <td><span>{t(`oiceError.code.${code}`, { blockIndex, blockId, value })}</span></td>
+            </tr>
+            {!!value &&
+              <tr>
+                <td><span>{t('oiceError.value')}</span></td>
+                <td><span>{value}</span></td>
+              </tr>
+            }
+          </table>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ErrorAttributeList.propTypes = {
+  t: PropTypes.func.isRequired,
+  blockId: PropTypes.number.isRequired,
+  blockIndex: PropTypes.number.isRequired,
+  macroName: PropTypes.string.isRequired,
+  errors: PropTypes.arrayOf(PropTypes.shape({
+    attributeName: PropTypes.string.isRequired,
+    code: PropTypes.number.isRequired,
+    value: PropTypes.string,
+  })),
+};
+
 const OiceErrorList = (props) => {
   const { t, errors } = props;
 
-  const renderErrorMessages = (errorsObj) => {
-    const errorMessagesComponent = Object.keys(errorsObj).map((key, index) => {
-      const errorMessage = errorsObj[key];
-      return (
-        <li key={index} className="error-attribute-item">
-          <div><span >{t('oiceError.attributeName')}</span> {key}</div>
-          <div><span>{t('oiceError.messageLabel')}</span> {errorMessage}</div>
-        </li>);
-    });
-    return errorMessagesComponent;
-  };
-
-  const render = errors.map((error, i) => {
-    console.log(error);
-    const errorBlockId = error.blockId;
-    const errorsObj = error.errors;
-    return (
-      <li className="error-block-item" key={i}>
-        <span>{t('oiceError.errorBlockId')}</span> #{errorBlockId}
-        <ul>
-          {renderErrorMessages(errorsObj)}
-        </ul>
-      </li>
-    );
-  });
-  return <ul>{render}</ul>;
+  return (
+    <ul>
+      {errors.map((error) => {
+        const { id, macroName, order } = error.block;
+        const blockIndex = order + 1;
+        const errorAttributes = error.errors;
+        return (
+          <li key={id} className="error-block-item">
+            <h1>{t('oiceError.errorBlock')} {blockIndex} #{id} - {t(`macro:${macroName}._title`)}</h1>
+            <div>
+              <ErrorAttributeList
+                blockId={id}
+                blockIndex={blockIndex}
+                macroName={macroName}
+                errors={errorAttributes}
+                t={t}
+              />
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 OiceErrorList.propTypes = {
