eb541cbdd4475782b40077e1c775e404942ef72e
Miles Wong <miles@oice.com>
2017-11-30 16:09:33 +0800

    🐛 Move add_library Intercom event to the right place

--- src/editor/actions/library.js
+++ src/editor/actions/library.js
@@ -8,52 +8,38 @@ import * as IntercomUtils from 'common/utils/intercom';
 
 
 const fetchedLibraries = createAction('FETCHED_LIBRARIES');
-export const fetchLibraries = () => (dispatch) => APIHandler(dispatch,
+export const fetchLibraries = () => dispatch => APIHandler(dispatch,
   LibraryAPI.fetchLibraries()
   .then(libraries => dispatch(fetchedLibraries(libraries)))
 );
 
 
 export const addedLibrary = createAction('ADDED_LIBRARY');
-export const addLibrary = (newStoryName) => dispatch => APIHandler(dispatch,
+export const addLibrary = newStoryName => dispatch => APIHandler(dispatch,
   LibraryAPI.addLibrary(newStoryName)
-  .then(library => {
+  .then((library) => {
     dispatch(addedLibrary(library));
-
-    IntercomUtils.event('add_library', {
-      id: library.id,
-      name: library.name,
-      description: library.description,
-      is_public: library.isPublic,
-    });
   })
-);
-
-
-export const deletedStory = createAction('DELETED_LIBRARY');
-export const deleteLibrary = (libraryId) => dispatch => APIHandler(dispatch,
-  LibraryAPI.deleteLibrary(libraryId)
-  .then(() => dispatch(deletedStory(libraryId)))
 );
 
 
 // LIBRARIES
 
 const fetchedLibrariesByStory = createAction('FETCH_LIBRARIES_BY_STORY');
-export const fetchLibrariesByStory = (storyId) => (dispatch) => APIHandler(
+export const fetchLibrariesByStory = storyId => dispatch => APIHandler(
   dispatch,
   LibraryAPI.fetchLibrariesByStory(storyId)
   .then(libraries => dispatch(fetchedLibrariesByStory(libraries)))
 );
 
 const addedLibraryToStory = createAction('ADD_LIBRARY_TO_STORY');
-export const addLibraryToStory = (storyId, libraryId) => (dispatch) => APIHandler(
+export const addLibraryToStory = (storyId, libraryId) => dispatch => APIHandler(
   StoryAPI.addLibraryToStory(storyId, libraryId)
   .then(() => dispatch(fetchLibrariesByStory(storyId)))
 );
 
 export const removedLibraryFromStory = createAction('REMOVE_LIBRARY_FROM_STORY');
-export const removeLibraryFromStory = (storyId, libraryId) => (dispatch) => APIHandler(
+export const removeLibraryFromStory = (storyId, libraryId) => dispatch => APIHandler(
   StoryAPI.removeLibraryFromStory(storyId, libraryId)
   .then(() => dispatch(fetchLibrariesByStory(storyId)))
 );
