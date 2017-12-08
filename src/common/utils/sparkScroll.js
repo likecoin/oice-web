42d8788df4e360841c0b839fcbca6b6ec61f541b
Ng Wing Tat (David) <i@ngwingt.at>
2017-12-08 17:53:08 +0800

    ✨💄➕🔥 New license page

---
+++ src/common/utils/sparkScroll.js
@@ -0,0 +1,10 @@
+import sparkScroll from 'react-spark-scroll-rekapi';
+
+export const {
+  SparkProxy,
+  SparkScroll,
+} = sparkScroll({
+  invalidateAutomatically: true,
+});
+
+export default sparkScroll;
