6510a22238a81a9b274ac8575a82c5404490e215
William Chong <williamchong007@msn.com>
2018-02-12 16:16:13 +0800

    + Add basic security headers

--- src/server.jsx
+++ src/server.jsx
@@ -63,6 +63,13 @@ if (DEBUG) {
   }));
 }
 
+server.use((req, res, next) => {
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
+
 server.disable('x-powered-by');
 server.use(i18nMiddleware.handle(i18n));
 server.use(express.static(DIST_PATH));
